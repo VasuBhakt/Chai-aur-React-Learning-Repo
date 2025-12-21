5a6e8050b0e2ef703feaed0a658b5ffb3f3b4981
Swastik Bose <swastikbose31@gmail.com>
2025-12-21 17:03:32 +0530

    improved logging

--- currency-converter/src/hooks/useCurrencyInfo.ts
+++ currency-converter/src/hooks/useCurrencyInfo.ts
@@ -4,11 +4,12 @@ import { API_KEY } from "../secrets";
 function useCurrencyInfo(currency: string) {
     const [data, setData] = useState<Record<string, number>>({});
     useEffect(() => {
+        console.log("API CALLED for:", currency);
         fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`)
             .then((res) => res.json())
             .then((res) => setData(res.conversion_rates))
     }, [currency]);
-    console.log(data);
+
     return data;
 }
 
